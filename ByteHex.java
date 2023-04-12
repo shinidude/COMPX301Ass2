613bef46e6aa2fb9a98453d31abf2b5067dbbfe2
Shean Danes <sheandanes1978@gmail.com>
2023-04-12 20:05:39 +1200

    convertB2H added more code

--- ByteHex.java
+++ ByteHex.java
@@ -1,5 +1,9 @@
 class ByteHex {
-    public String convertB2H(byte[] bytes) {
-        return "";
+    public String [] convertB2H(byte[] bytes){
+        String [] stringArr = new String [bytes.length];  
+       for(int i= 0; i<bytes.length; i++){
+            stringArr[i] = String.format("%02x", bytes[i]);
+       }
+        return stringArr ;
     }
 }
