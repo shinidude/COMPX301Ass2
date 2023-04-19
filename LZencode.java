b10b14f3558fa150028a7abf4902c2d9252fb5d0
Joethy <michaelpeddie389@gmail.com>
2023-04-19 17:12:27 +1200

    Partial encode implementation
    It is mostly done, just need to actually use a trie structure, and make it so complete phrases get encoded. Output also needs to be done.

--- LZencode.java
+++ LZencode.java
@@ -1,40 +1,62 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 public class LZencode {
     public static void main(String[] args) {
         // reader object for standard input stream
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String hexChars=""; // var to store data in before encoding
         // try...catch... for any io errors
         try {
-            // vars for input
-            byte[] byteChars = new byte[0];
-            int charTracker = 0;
-            int iChar=0;
-            // while character read is not eos (end of stream)
-            while ((iChar=reader.read()) != -1) {
-                byteChars = incSize(byteChars); // increment byte array
-                byteChars[charTracker] = (byte)iChar; // cast and append char to byte array
-                charTracker++; // increment tracker
+            String line=""; // var to read stream into
+            // loop until end of stream, reading each line
+            while ((line=reader.readLine()) != null) {
+                // get each line as it's bytes in an array, and convert to hex, before turning into a string
+                hexChars += String.join("",ByteHex.convertB2H(line.getBytes()));
             }
-            // Convert bytes to hex
-            String[] hexChars = ByteHex.convertB2H(byteChars);
-            // System.out.println(Arrays.toString(hexChars)); // Testing output to ensure it is correct
-        } catch (IOException e) {
+            // System.out.println(hexChars); // Testing output to ensure it is correct
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         // encode here
-        
-    }
+        /*
+         * LOOP through each "nibble"
+         *     add to current phrase
+         *     WHILE tracker is not at end of dict
+         *         IF current phrase doesn't match known phrases
+         *             find most recently matching phrase
+         *             add to dictionary
+         *             reset current phrase
+         *         ENDIF
+         *     ENDWHILE
+         * ENDLOOP
+         */
+        String phrase="";
+        int tracker=0;
+        int lastPhrasePos = 0;
+        ArrayList<String> dictPhrases = new ArrayList<String>();
+        ArrayList<String> dictEncoded = new ArrayList<String>();
+        dictPhrases.add("");
+        dictEncoded.add("");
+        for (int i=0; i<hexChars.length(); i++) {
+            phrase += String.valueOf(hexChars.charAt(i));
 
-    public static byte[] incSize(byte[] array) {
-        byte[] out = new byte[array.length+1];
-        System.arraycopy(array, 0, out, 0, array.length);
+            if (dictPhrases.contains(phrase)) {
+                lastPhrasePos = dictPhrases.indexOf(phrase);
+            } else {
+                if (phrase.length() == 1) {
+                    lastPhrasePos = 0;
+                }
+                dictPhrases.add(phrase);
+                dictEncoded.add(lastPhrasePos + " " + String.valueOf( phrase.charAt( phrase.length()-1 ) ));
+                phrase = "";
+            }
+        }
 
-        return out;
+        System.out.println(dictPhrases.toString());
+        System.out.println(dictEncoded.toString());
     }
 }
